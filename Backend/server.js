72c8f188ef18292c497430486d74f447f6744fad
Amit-cl <amitkumar2182004@gmail.com>
2026-06-02 14:43:49 +0530

    frontend setup

--- Backend/server.js
+++ Backend/server.js
@@ -13,11 +13,10 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-  })
-);
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
